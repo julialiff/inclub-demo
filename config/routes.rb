188c4fd82c54ab9e92047796956237db21d73040
Julia Litvinoff Justus <julialiff@hotmail.com>
2017-06-21 22:38:10 -0300

    design

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :baladas
   resources :cadastros
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'baladas#index'
+  root to: 'festa#index'
   get '/administrador', to: 'administrador#show'
   resources :administradors
   resources :festas
