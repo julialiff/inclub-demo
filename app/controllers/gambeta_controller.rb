d5a90bd1c256302b94ccc0ce10f77f1350ce15f6
Julia Litvinoff Justus <julialiff@hotmail.com>
2017-06-26 00:16:39 +0000

    Server configs

--- app/controllers/gambeta_controller.rb
+++ app/controllers/gambeta_controller.rb
@@ -1,7 +1,7 @@
 class GambetaController < ActionController::Base
-  def show
-    @connection = ActiveRecord::Base.establish_connection
-    sql = ""
-    @result = @connection.connection.execute(sql)
-  end
+#  def show
+#    @connection = ActiveRecord::Base.establish_connection
+#    sql = ""
+#    @result = @connection.connection.execute(sql)
+#  end
 end
--- db/migrate/20170611041913_create_festa.rb
+++
@@ -1,8 +0,0 @@
-class CreateFesta < ActiveRecord::Migration[5.0]
-  def change
-    create_table :festa do |t|
-
-      t.timestamps
-    end
-  end
-end
